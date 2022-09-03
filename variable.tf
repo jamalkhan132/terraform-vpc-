ab2aea87a375869714e73ded14230e75fdac8e59
Your Name <you@example.com>
2022-09-03 12:07:10 +0530

    vpc terraformm code

---
+++ variable.tf
@@ -0,0 +1,15 @@
+variable public-cidr {
+
+  default = ["10.0.1.0/24","10.0.2.0/24","10.0.3.0/24"]
+ 
+}
+variable private-cidr {
+
+  default = ["10.0.4.0/24","10.0.5.0/24","10.0.6.0/24"]
+ 
+}
+variable data-cidr {
+
+  default = ["10.0.7.0/24","10.0.8.0/24","10.0.9.0/24"]
+ 
+}
